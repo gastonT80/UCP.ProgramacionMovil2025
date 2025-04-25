9fa741952987bbaf543baff8673774c3664d43d8
GastonT80 <208198098+gastonT80@users.noreply.github.com>
2025-04-25 14:52:11 -0300

    agregar script y se realizan pruebas.

--- Ace/Assets/Scrript/Ace.cs
+++ Ace/Assets/Scrript/Ace.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Ace : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float speed = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb= GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 vc = Input.acceleration;
+        vc= Quaternion.Euler(90,0,0)*vc;
+        rb.AddForce(vc*speed);
+
     }
 }
